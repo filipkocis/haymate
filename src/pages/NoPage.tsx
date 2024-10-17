6a23591ce7a9d386536b19c20b981fbd328bf7ce
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-10-18 01:34:03 +0200

    feat: nopage page

--- src/pages/NoPage.tsx
+++ src/pages/NoPage.tsx
@@ -1,7 +1,14 @@
+import Flushed from "/src/assets/flushed_nobg.png";
+
 export default function NoPage() {
   return (
-    <div>
-      <h1>404: Page Not Found</h1>
+    <div className="flex gap-8 items-center justify-center flex-col grow">
+      <img 
+        src={Flushed} 
+        alt="Flushed emoji" 
+        className="w-40 h-40 animate-goofball"
+      />
+      <p className="text-4xl">404: Page Not Found</p>
     </div>
   )
 }
