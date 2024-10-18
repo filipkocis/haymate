f7b3fcfa47437d7204a6b9b2893c759811790a25
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-10-19 01:46:21 +0200

    feat: debounce util

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,3 +5,10 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
+export function debounce(fn: (...args: any[]) => any, delay: number) {
+  let timeoutId: ReturnType<typeof setTimeout>;
+  return function (...args: any[]) {
+    if (timeoutId) clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => fn(...args), delay);
+  };
+}
