401370a2a8858c32a4d7e0013727dce92a6a3a95
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-10-19 18:41:02 +0200

    feat: chatswitch component

---
+++ src/pages/messages/components/ChatSwitch.tsx
@@ -0,0 +1,16 @@
+import { cn } from "/src/lib/utils"
+
+export type Selected = 'messages' | 'requests' | 'x'
+export default function ChatSwitch({ selected, setSelected, variant }: { selected: Selected, setSelected: (selected: Selected) => void, variant: Selected }) {
+  return (
+    <button 
+      className={cn(
+        "p-2 text-center uppercase min-w-[110px] duration-300 transition-all",
+        selected == variant && "text-black")
+      } 
+      onClick={() => setSelected(variant)}
+    >
+      variant
+    </button>  
+  )
+}
