38df30ff3be24d9661e1fcbc856c8c604820ec30
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-10-19 21:11:13 +0200

    fix: add missing overflowhidden to avatar

--- src/pages/messages/components/ChatCard.tsx
+++ src/pages/messages/components/ChatCard.tsx
@@ -1,7 +1,7 @@
 export default function ChatCard() {
   return (
     <div className="grid grid-cols-[auto,1fr] items-center gap-4 p-3 rounded-md shadow-[0_0_0.5rem_1px_hsl(var(--border))]">
-      <div className="w-14 h-14 rounded-full shadow-lg">
+      <div className="w-14 h-14 rounded-full overflow-hidden shadow-lg">
         <img src="https://randomuser.me/api/portraits/men/1.jpg" className="aspect-[1/1] h-full w-full object-cover object-center" />
       </div>
       <div className="flex flex-col gap-1">
