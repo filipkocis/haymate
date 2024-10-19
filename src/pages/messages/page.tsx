23126b0315be982fa7d93f24a4f254fdc9812b5f
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-10-19 18:31:20 +0200

    fix: fix x-scroll in messages

--- src/pages/messages/page.tsx
+++ src/pages/messages/page.tsx
@@ -4,8 +4,8 @@ import OpenChatView from "./components/OpenChatView"
 export default function Messages() {
   return (
     <div className="animate-fade-in-page grid lg:grid-cols-[auto,1fr] gap-6 overflow-hidden">
-      <ChatsView className="border overflow-auto overflow-x-hidden p-1" /> 
-      <OpenChatView className="border overflow-auto p-1" />
+      <ChatsView className="overflow-auto overflow-x-hidden p-2" /> 
+      <OpenChatView className="overflow-auto overflow-x-hidden p-2" />
     </div>
   )
 }
