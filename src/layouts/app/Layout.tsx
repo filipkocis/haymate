0ad8d6843d396d1949232fdbb626c87d7b4dd3a8
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-10-22 00:04:40 +0200

    feat: redirect to login with no session

--- src/layouts/app/Layout.tsx
+++ src/layouts/app/Layout.tsx
@@ -1,12 +1,24 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import AppLayout from "./AppLayout";
+import { useSession } from "@/context/SessionProvider";
+import { useEffect } from "react";
 
 export default function Layout() {
+  const navigate = useNavigate()
+  const location = useLocation()
+  const { session } = useSession()
+  const noSession = session === null
+
+  useEffect(() => {
+    if (noSession && location.pathname !== "/login") navigate("/login")
+  }, [session, location.pathname])
+
   return (
     <div className="grid md:grid-cols-[auto,1fr] grid-rows-[auto,1fr] h-screen overflow-hidden">
       <AppLayout />
       <main className="grid overflow-hidden">
-        <Outlet />
+        {session && <Outlet />}
+        {(noSession && location.pathname === "/login") && <Outlet />}
       </main>
     </div>
   )
