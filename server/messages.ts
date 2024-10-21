05ad88914439889ef501331a42515690eac07835
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-10-22 00:08:56 +0200

    feat(server): implement messages store

---
+++ server/messages.ts
@@ -0,0 +1,60 @@
+import Store from "./store.ts";
+
+type ChatID = string
+export type Message = {
+  id: string
+  text: string
+  timestamp: number
+  author: string
+}
+
+type MessageStore = Store<string, Message>
+
+export default class ChatStore extends Store<ChatID, MessageStore> {
+  constructor() {
+    super()
+  }
+
+  override get(chatId: ChatID): MessageStore {
+    const chat = super.get(chatId)
+    if (!chat) {
+      const chat = new Store<string, Message>()
+      super.set(chatId, chat)
+      return chat
+    }
+    return chat
+  }
+
+  getMessages(chatId: ChatID, cursor = 0, limit = 50): Message[] {
+    const chat = this.get(chatId)
+    const messages = chat.values().toArray().toSorted((a, b) => a.timestamp - b.timestamp)
+    return messages.slice(cursor, cursor + limit)
+  }
+
+  addMessage(chatId: ChatID, message: Message) {
+    const chat = this.get(chatId)
+    chat.set(message.id, message)
+  }
+
+  deleteMessage(chatId: ChatID, messageId: string) {
+    const chat = this.get(chatId)
+    chat.delete(messageId)
+  }
+
+  clearChat(chatId: ChatID) {
+    const chat = this.get(chatId)
+    chat.clear()
+  }
+
+  id(idA: string, idB: string): ChatID {
+    return [idA, idB].toSorted().join(':')
+  }
+
+  allChats(userId: string): ChatID[] {
+    return Array.from(this.keys())
+      .filter(key => {
+        const [idA, idB] = key.split(':')
+        return idA === userId || idB === userId
+      })
+  }
+}
