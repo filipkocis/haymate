dc72de90256896aa5298955d0cee4dea2df6e9ee
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-10-23 13:08:26 +0200

    feat: implement loader component

---
+++ src/components/Loader.tsx
@@ -0,0 +1,9 @@
+export default function Loader() {
+  return (
+    <div className="flex items-center justify-center h-full">
+      <div className="animate-spin h-20 w-20">
+        <img src="/hay.png" alt="Hay Icon" width={80} height={80} />
+      </div>
+    </div>
+  )
+}
