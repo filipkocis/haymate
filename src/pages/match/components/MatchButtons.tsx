d7f0dfab92c72bda2442725c50fdd00a9ba2bbdc
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2024-10-23 15:19:00 +0200

    feat(match): add match button actions

--- src/pages/match/components/MatchButtons.tsx
+++ src/pages/match/components/MatchButtons.tsx
@@ -1,21 +1,24 @@
 import { LucideHeart, LucideHeartPulse, LucideX } from "lucide-react";
-import { cn } from "/src/lib/utils";
+import { cn } from "@/lib/utils";
 
 const SIZE = 34;
-export default function MatchButtons({ onCancel, className }: any) {
+export default function MatchButtons(
+  { onNext, onLike, onLove, className }: 
+  { onNext: () => void, onLike: () => void, onLove: () => void, className?: string }
+) {
   return (
     <div className={cn(
       "pt-2 sm:pt-10 pb-3 sm:pb-10",
       "[&_button]:shadow-[0_0_1rem_0.2rem_hsl(var(--border))] [&_svg]:duration-300 [&>*]:transition-all [&_button]:h-auto [&_button]:rounded-full [&_button]:p-3 [&_button]:border flex gap-5 justify-center",
       className
     )}>
-      <button onClick={onCancel} className="group text-red-400 hover:bg-red-400/10">
+      <button onClick={onNext} className="group text-red-400 hover:bg-red-400/10">
         <LucideX size={SIZE} className="group-hover:scale-110" />
       </button>
-      <button className="group text-pink-400 hover:bg-pink-400/10">
+      <button onClick={onLike} className="group text-pink-400 hover:bg-pink-400/10">
         <LucideHeart size={SIZE} className="group-hover:scale-110" />
       </button>
-      <button className="group text-primary hover:bg-primary/10">
+      <button onClick={onLove} className="group text-primary hover:bg-primary/10">
         <LucideHeartPulse size={SIZE} className="group-hover:scale-110" />
       </button>
     </div>
--- src/pages/match/page.tsx
+++ src/pages/match/page.tsx
@@ -6,12 +6,12 @@ import { Profile } from "./types";
 import Loader from "@/components/Loader";
 
 export default function Match() {
-  const [switcheroo, setSwitcheroo] = useState(false)
+  const [switcheroo, setSwitcheroo] = useState<"next" | "like" | "love" | null>(null)
   const [current, setCurrent] = useState<Profile>()
   const [next, setNext] = useState<Profile>()
 
-  function cancelHandler() {
-    setSwitcheroo(true)
+  function matchActionHandler(value: "next" | "like" | "love") {
+    setSwitcheroo(value)
   }
 
   useEffect(() => {
@@ -41,8 +41,10 @@ export default function Match() {
     let timer: number;
 
     async function load() {
+      if (!switcheroo) return;
+
       try {
-        const res = await fetch("/api/match?next=true", { signal: controller.signal })
+        const res = await fetch(`/api/match?${switcheroo}=true`, { signal: controller.signal })
         if (!res.ok) return
         const data = await res.json()
 
@@ -52,7 +54,7 @@ export default function Match() {
             return data.next
           })
 
-          setSwitcheroo(false)
+          setSwitcheroo(null)
         }, 1000)
       } catch (error) {
         console.error(error)
@@ -81,7 +83,12 @@ export default function Match() {
           switcheroo && "duration-500 transition-all opacity-0 -translate-x-1/3 z-10 rotate-[-30deg]",
         )} 
       />
-      <MatchButtons onCancel={cancelHandler} className="z-0 bg-background" />
+      <MatchButtons 
+        onNext={() => matchActionHandler('next')} 
+        onLike={() => matchActionHandler('like')} 
+        onLove={() => matchActionHandler('love')} 
+        className="z-0 bg-background" 
+      />
     </div>
   )
 }
